9174f582ede29c976f760dcafb4580aaeb4ec7a1
Rawan <rawan.nujoom@gmail.com>
2020-01-19 04:22:32 +0200

    add config file

---
+++ config.js
@@ -0,0 +1,15 @@
+const config = {
+    port: 2000,
+    pgDB: {
+		connStr: 'X',
+		database: 'X',
+		host: 'X',
+		port: 'X',
+		user: 'X',
+		pass: 'X',
+		poolSize:2,
+		poolIdleTimeout: 60000
+	}
+};
+
+module.exports = config;
