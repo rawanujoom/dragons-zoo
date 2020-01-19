b9da2c97c877bf7ab8e06ef0f5204cfeeb071644
Rawan <rawan.nujoom@gmail.com>
2020-01-19 04:25:02 +0200

    app backend setup

---
+++ app.js
@@ -0,0 +1,35 @@
+const staticPath = __dirname + '/client/dist';
+const bodyParser = require('body-parser');
+const express = require('express');
+var app = express();
+
+app
+    /*Register Static content path*/
+    .use(express.static(staticPath))
+
+    .use(bodyParser.json({limit: '50mb'}))
+    .use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+    /*register api routers*/
+    .use('/dragon', require('./routes/dragon'))
+    .use('/location', require('./routes/location'))
+    /*catch 404*/
+    .use((req, res, next)=> {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    })
+    /*Error Handling */
+    .use((err, req, res, next)=> {
+        if(err.status != 404) {
+            console.error(err);
+        }
+        res.status(err.status || 500).json({
+            error: {}
+        });
+    })
+    /*Default route*/
+    .use((req, res)=>{
+      res.sendFile(staticPath + '/index.html');
+    });
+
+module.exports = app;
